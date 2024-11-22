58da2a3e10f80c23d6df5b8b9b89f5bba49d10f7
Ravi Padache <42465089+ravi357767@users.noreply.github.com>
2024-11-22 18:48:57 +0530

    Created Main Container with Columns Inside and given gradient colors

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,44 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("To Do List")),
+        title: Center(
+            child: Text(
+          "To Do List",
+          style: TextStyle(
+            fontFamily: 'Mainfont',
+            fontWeight: FontWeight.w800,
+            fontSize: 30,
+          ),
+        )),
       ),
       body: Container(
+        padding: EdgeInsets.all(8),
+        width: double.infinity,
+        height: 700,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xffff930f),
+              Color(0xfffff95b),
+            ],
+          ),
+        ),
         child: Column(
-          children: [],
+          children: [
+            Container(
+              width: double.infinity,
+              height: 150,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xff0061ff),
+                    Color(0xff60efff),
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ],
         ),
       ),
     );
